c2aecdb106cf5622ade9a1e0fe387dc43bda8354
Austin Proctor <drproctor@fb.com>
2017-04-29 20:38:50 -0700

    Delcaring global vars

--- AutoFiler/filer.rb
+++ AutoFiler/filer.rb
@@ -1,12 +1,27 @@
+@doc_arr = [
+  '.csv',
+  '.txt',
+  '.xls',
+]
+@package_arr = [
+  '.dmg',
+  '.exe',
+]
+@pic_arr = [
+  '.jpg',
+  '.png',
+  '.ttf',
+]
+
 # Define functions here
 def AutoFiler(uname)
   contents = Dir.entries("/Users/#{uname}/Downloads")
   contents.each do |f|
-    # applications
-    if f.any? { |x| ['.xls', '.csv', '.txt'].include?(x) }
+    if @doc_arr.any? { |k| f.include? k }
       puts "Move this to documents: #{f}"
-    end
+    elsif @package_arr.any? { |k| f.include? k }
+      puts "Move this to App_Installs: #{f}"
+    elsif @pic_arr.any? { |k| f.include? k }
+      puts "Move this to Pictures: #{f}"
   end
 end
-
-AutoFiler('klborders')
